2839b4355965e4935717a24e158851bfb41819cc
Tara Chakkithara <113033661+redpinecube@users.noreply.github.com>
2025-09-15 11:54:01 -0400

    updated log.go to include log level filtering for each subhandler (#58)
    Signed-off-by: redpinecube <tara.chakkithara@icloud.com>

--- src/log.go
+++ src/log.go
@@ -11,41 +11,62 @@ import (
 
 const LogConfigFilePath = "../config/log.yaml"
 
+var levelMap = map[string]slog.Level{
+	"DEBUG": slog.LevelDebug,
+	"INFO":  slog.LevelInfo,
+	"WARN":  slog.LevelWarn,
+	"ERROR": slog.LevelError,
+}
+
 type MultiHandler struct {
 	subHandlers []slog.Handler
-	level       slog.Level
+}
+
+type OutputType struct {
+	Type  string `yaml:"type"`
+	Level string `yaml:"level"`
 }
 
 type LogConfig struct {
 	Output struct {
-		Types     []string `yaml:"types"`
-		Directory string   `yaml:"directory"`
+		Types     []OutputType `yaml:"types"`
+		Directory string       `yaml:"directory"`
 	} `yaml:"output"`
 	Components []string `yaml:"components"`
 }
 
-func NewMultiHandler(level slog.Level, writers []io.Writer) *MultiHandler {
+func NewMultiHandler(writerLevels map[io.Writer]slog.Level) *MultiHandler {
 	var handlers []slog.Handler
 
-	for _, writer := range writers {
-		handlers = append(handlers, slog.NewJSONHandler(writer, nil))
+	for writer, level := range writerLevels {
+		handlers = append(handlers, slog.NewJSONHandler(writer, &slog.HandlerOptions{Level: level}))
 	}
 
 	m := MultiHandler{
-		level:       level,
 		subHandlers: handlers,
 	}
 	return &m
 }
 
-func (h *MultiHandler) Enabled(ctx context.Context, level slog.Level) bool { return h.level <= level }
-func (h *MultiHandler) WithAttrs(attrs []slog.Attr) slog.Handler           { return h }
-func (h *MultiHandler) WithGroup(name string) slog.Handler                 { return h }
+func (h *MultiHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	for _, handler := range h.subHandlers {
+		if handler.Enabled(ctx, level) {
+			return true
+		}
+	}
+	return false
+
+}
+
+func (h *MultiHandler) WithAttrs(attrs []slog.Attr) slog.Handler { return h }
+func (h *MultiHandler) WithGroup(name string) slog.Handler       { return h }
 func (h *MultiHandler) Handle(ctx context.Context, record slog.Record) error {
 	var err error
 	for _, handler := range h.subHandlers {
-		if out := handler.Handle(ctx, record); out != nil {
-			err = out
+		if handler.Enabled(ctx, record.Level) {
+			if out := handler.Handle(ctx, record); out != nil {
+				err = out
+			}
 		}
 	}
 	return err
@@ -72,11 +93,11 @@ func createLogger(component string) (*slog.Logger, error) {
 	if err != nil {
 		return nil, err
 	}
-	var writers []io.Writer
+	writerLevels := make(map[io.Writer]slog.Level)
 	for _, t := range logConfig.Output.Types {
-		switch t {
+		switch t.Type {
 		case "stdout":
-			writers = append(writers, os.Stdout)
+			writerLevels[os.Stdout] = levelMap[t.Level]
 		case "file":
 			directory := logConfig.Output.Directory
 
@@ -89,11 +110,10 @@ func createLogger(component string) (*slog.Logger, error) {
 			if err != nil {
 				return nil, err
 			}
-			writers = append(writers, file)
-
+			writerLevels[file] = levelMap[t.Level]
 		}
 	}
-	handler := NewMultiHandler(slog.LevelInfo, writers)
+	handler := NewMultiHandler(writerLevels)
 	logger := slog.New(handler).With("component", component)
 	return logger, nil
 }
