9920b2d3d7947c320f10bd524c26b1e3043e0dc6
redpinecube <tara.chakkithara@icloud.com>
2025-10-05 17:36:52 -0500

    added job states
    Signed-off-by: redpinecube <tara.chakkithara@icloud.com>

--- src/util.go
+++ src/util.go
@@ -14,6 +14,16 @@ const (
 	RetryDelay          = 5 * time.Second
 )
 
+type JobState string
+
+const (
+	JobStateScheduled  JobState = "Scheduled"
+	JobStateInProgress JobState = "InProgress"
+	JobStateSuccess    JobState = "Success"
+	JobStateError      JobState = "Error"
+	JobStateFailure    JobState = "Failure"
+)
+
 type Job struct {
 	ID          string                 `json:"id"`
 	Type        string                 `json:"type"`
@@ -21,6 +31,7 @@ type Job struct {
 	Retries     int                    `json:"retries"`
 	Created     time.Time              `json:"created"`
 	RequiredGPU string                 `json:"gpu"`
+	JobState    JobState               `json:"job_state"`
 }
 
 type SupervisorState string
