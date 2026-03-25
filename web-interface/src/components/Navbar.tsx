ce01cc9986ab91187e9c3da0d15f5a0fb628e086
notjackl3 <huuanducle@gmail.com>
2026-03-25 00:08:32 -0400

    resized navbar

--- web-interface/src/components/Navbar.tsx
+++ web-interface/src/components/Navbar.tsx
@@ -21,17 +21,19 @@ export default function Navbar() {
   const user = getUser();
 
   return (
-    <nav className="flex gap-10 justify-center text-3xl border rounded-xl m-2 p-2">
-      <Navlink href="/dashboard" text="Dashboard"/>
-      <Navlink href="/machines" text="Machines"/>
-      <Navlink href="/jobs" text="Jobs"/>
+    <nav className="flex items-center text-lg border rounded-xl mx-auto mt-8 mb-4 px-8 py-2 w-fit gap-8">
+      <div className="flex gap-8">
+        <Navlink href="/dashboard" text="Dashboard"/>
+        <Navlink href="/machines" text="Machines"/>
+        <Navlink href="/jobs" text="Jobs"/>
+      </div>
       <div className="relative">
         <div className="flex items-center cursor-pointer py-1 px-2 rounded-lg hover:bg-gray-100 transition-colors duration-200" onClick={() => setDropdown(!dropdown)}>
           { user.username }
-          <svg className="w-10 h-10" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 15L7 10H17L12 15Z" fill="#1D1B20"/>
           </svg>
-          <img src={ user.profilePicture } alt="Profile Picture" className="w-10 h-10"></img>
+          <img src={ user.profilePicture } alt="Profile Picture" className="w-7 h-7 rounded-full"></img>
         </div>
         { dropdown &&
             <div className="absolute top-full right-0 border-1 rounded-b m-2 p-1 pr-3 pl-3 text-xl">
